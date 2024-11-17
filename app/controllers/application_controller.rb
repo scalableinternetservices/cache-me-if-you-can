8fd4735989788fb038320a68afc6a1ca3c707410
Harold <haroldmo23@gmail.com>
2024-11-16 22:44:28 -0800

    Added student model, login, logout, and homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "Hello, world!"
+  helper_method :current_student
+
+  def current_student
+    @current_student ||= Student.find_by(id: session[:student_id]) if session[:student_id]
   end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    def new
+    end
+  
+    def create
+      @student = Student.find_by(username: params[:username])
+      if @student&.password == params[:password]
+        session[:student_id] = @student.id
+        redirect_to root_path, notice: "Logged in successfully."
+      else
+        flash.alert = "Invalid username or password."
+        render :new, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      session[:student_id] = nil
+      redirect_to login_path, notice: "Logged out successfully."
+    end
+  end
+  
