0dbfced2c70a21a15eb382a46c95ce6b86c0bdbf
Harold <haroldmo23@gmail.com>
2024-11-25 22:43:54 -0800

    added functionality to show the number of comments for every review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :student, optional: true
   belongs_to :professor
   belongs_to :course
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :content, presence: true
   validates :rating, presence: true, inclusion: { in: 1..5 }
